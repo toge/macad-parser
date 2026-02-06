318c91a941b38730544c69be7512de7a2788bc10
toge <toge.mail@gmail.com>
2026-02-07 03:15:59 +0900

    testソースファイルの名前を関数名に合わせた

--- test/test_mac.cpp
+++ test/test_mac.cpp
@@ -1,33 +1,35 @@
+#include <string>
+
 #include "catch2/catch_all.hpp"
 
 #include "macad-parser.hpp"
 
-TEST_CASE("mac address parser") {
-  auto const a = std::string_view{"AA:BB:CC:DD:EE:FF               "}.substr(0, 17);
+TEST_CASE("mac address parser safe") {
+  auto const a = std::string{"AA:BB:CC:DD:EE:FF"};
 
-  auto const result = macad_parser::parse_mac_address_unsafe(a);
+  auto const result = macad_parser::parse_mac_address(a);
   REQUIRE(result.has_value());
   REQUIRE(result.value() == 0xAABBCCDDEEFF);
 }
 
-TEST_CASE("mac address parser (non symmetric)") {
-  auto const a = std::string_view{"01:23:45:67:89:AB               "}.substr(0, 17);
+TEST_CASE("mac address parser safe (non symmetric)") {
+  auto const a = std::string{"01:23:45:67:89:AB"};
 
-  auto const result = macad_parser::parse_mac_address_unsafe(a);
+  auto const result = macad_parser::parse_mac_address(a);
   REQUIRE(result.has_value());
   REQUIRE(result.value() == 0x0123456789AB);
 }
 
-TEST_CASE("mac address parser (strict validation)") {
+TEST_CASE("mac address parser safe (strict validation)") {
   SECTION("rejects wrong delimiters") {
-    auto const a = std::string_view{"01-23-45-67-89-AB               "}.substr(0, 17);
-    auto const result = macad_parser::parse_mac_address_unsafe<macad_parser::parse_mac_options_strict>(a);
+    auto const a = std::string{"01-23-45-67-89-AB"};
+    auto const result = macad_parser::parse_mac_address<macad_parser::parse_mac_options_strict>(a);
     REQUIRE_FALSE(result.has_value());
   }
 
-  SECTION("rejects non-hex characters") {
-    auto const a = std::string_view{"01:23:45:67:89:AG               "}.substr(0, 17);
-    auto const result = macad_parser::parse_mac_address_unsafe<macad_parser::parse_mac_options_strict>(a);
+  SECTION("rejects non-hex safe characters") {
+    auto const a = std::string{"01:23:45:67:89:AG"};
+    auto const result = macad_parser::parse_mac_address<macad_parser::parse_mac_options_strict>(a);
     REQUIRE_FALSE(result.has_value());
   }
 }
@@ -36,17 +38,17 @@ struct opt_delimiter : public macad_parser::parse_mac_options_strict {
   static constexpr char delimiter = '-';
 };
 
-TEST_CASE("custom delimiter (strict validation)") {
-  SECTION("rejects custom delimiters") {
-    auto const a = std::string_view{"01-23-45-67-89-AB               "}.substr(0, 17);
-    auto const result = macad_parser::parse_mac_address_unsafe<opt_delimiter>(a);
+TEST_CASE("custom delimiter safe (strict validation)") {
+  SECTION("accepts custom delimiter") {
+    auto const a = std::string{"01-23-45-67-89-AB"};
+    auto const result = macad_parser::parse_mac_address<opt_delimiter>(a);
     REQUIRE(result.has_value());
     REQUIRE(result.value() == 0x0123456789AB);
   }
 
   SECTION("rejects wrong delimiters") {
-    auto const a = std::string_view{"01:23:45:67:89:AF               "}.substr(0, 17);
-    auto const result = macad_parser::parse_mac_address_unsafe<opt_delimiter>(a);
+    auto const a = std::string{"01:23:45:67:89:AF"};
+    auto const result = macad_parser::parse_mac_address<opt_delimiter>(a);
     REQUIRE_FALSE(result.has_value());
   }
 }
---
+++ test/test_mac_unsafe.cpp
@@ -0,0 +1,52 @@
+#include "catch2/catch_all.hpp"
+
+#include "macad-parser.hpp"
+
+TEST_CASE("mac address parser") {
+  auto const a = std::string_view{"AA:BB:CC:DD:EE:FF               "}.substr(0, 17);
+
+  auto const result = macad_parser::parse_mac_address_unsafe(a);
+  REQUIRE(result.has_value());
+  REQUIRE(result.value() == 0xAABBCCDDEEFF);
+}
+
+TEST_CASE("mac address parser (non symmetric)") {
+  auto const a = std::string_view{"01:23:45:67:89:AB               "}.substr(0, 17);
+
+  auto const result = macad_parser::parse_mac_address_unsafe(a);
+  REQUIRE(result.has_value());
+  REQUIRE(result.value() == 0x0123456789AB);
+}
+
+TEST_CASE("mac address parser (strict validation)") {
+  SECTION("rejects wrong delimiters") {
+    auto const a = std::string_view{"01-23-45-67-89-AB               "}.substr(0, 17);
+    auto const result = macad_parser::parse_mac_address_unsafe<macad_parser::parse_mac_options_strict>(a);
+    REQUIRE_FALSE(result.has_value());
+  }
+
+  SECTION("rejects non-hex characters") {
+    auto const a = std::string_view{"01:23:45:67:89:AG               "}.substr(0, 17);
+    auto const result = macad_parser::parse_mac_address_unsafe<macad_parser::parse_mac_options_strict>(a);
+    REQUIRE_FALSE(result.has_value());
+  }
+}
+
+struct opt_delimiter : public macad_parser::parse_mac_options_strict {
+  static constexpr char delimiter = '-';
+};
+
+TEST_CASE("custom delimiter (strict validation)") {
+  SECTION("rejects custom delimiters") {
+    auto const a = std::string_view{"01-23-45-67-89-AB               "}.substr(0, 17);
+    auto const result = macad_parser::parse_mac_address_unsafe<opt_delimiter>(a);
+    REQUIRE(result.has_value());
+    REQUIRE(result.value() == 0x0123456789AB);
+  }
+
+  SECTION("rejects wrong delimiters") {
+    auto const a = std::string_view{"01:23:45:67:89:AF               "}.substr(0, 17);
+    auto const result = macad_parser::parse_mac_address_unsafe<opt_delimiter>(a);
+    REQUIRE_FALSE(result.has_value());
+  }
+}
